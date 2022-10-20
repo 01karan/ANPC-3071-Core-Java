876c5a2ebbb4ecde76d596e75baded8ab0ed5dca
01karan <115773537+01karan@users.noreply.github.com>
2022-10-20 11:24:08 +0530

    Add files via upload

---
+++ Discount 1.java
@@ -0,0 +1,50 @@
+//WAP to give 10% discount to the user whose shopping bill id greater than 2000.(ask the qty , cost of one unit is 540)//
+//mr.karan.singh
+//20-10-2022
+//import packages 
+
+import java.util.Scanner;
+//declaring a class
+class Discount 1
+{
+	//main started
+	public static void main(String args[])
+	{
+		//declaring the object of scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//declaring variables
+		float Discount;
+		char  char ;
+		
+		//taking input
+		System.out.println("Enter the prize:");
+		Discount= sc.nextFloat();
+		
+		
+		//checking Discount to cal grade
+		if(Discount>=2000)
+	     	 grade='user whose Shopping in high level ';
+		 else if(Discount>=1500)
+			  grade='user whose Shopping in high level';
+		      else if(Discount>=1000)
+				    grade='you have  discout ';
+	                 else if(Discount>=540)
+                           grade='no Discount';
+                           else
+                            
+						 
+			   			
+		//end of ifelse	
+        if(Discount=='') // comparing
+	      System.out.println("s*markedprice") 2000;
+	   else
+            System.out.println("amount after discount:"+ discount);		   
+	
+		
+		    
+	}
+		
+	// end of main
+}
+// end of class 
